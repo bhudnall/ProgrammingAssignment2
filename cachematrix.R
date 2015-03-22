32beae7e865ae1654af0c5a0b1545169a93017c7
bhudnall <brian.hudnall@gmail.com>
2015-03-21 18:51:16 -0700

    add first function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program allows you to create, access and edit
+## a matrix. The funciton also allows to to find the 
+## inverse of the matrix. The program assumes that
+## the matrix provided can have an inverse.
 
-## Write a short comment describing this function
+## This function essentially allows you to create,
+## edit, and access a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixInverse <- NULL ## creates an empty matrix
+  setMatrix <- function(y) { ## mutator function that sets the matrix value
+    myMatrix <<- y
+    matrixInverse <<- NULL
+  }
+  getMatrix <- function() myMatrix ## accessor function
+  setMatrixInverse <- function(theInverse) matrixInverse <<- theInverse
+  getMatrixInverse <- function() matrixInverse
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse) 
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix.
+## It assumes that the matrix creates in makeCacheMatrix
+## has an inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
 }
